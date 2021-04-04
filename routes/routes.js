64ddda381a352013f8b5982728810172702d0299
Shon Hatcher <shonhatcher@hotmail.com>
2021-04-04 08:52:14 -0400

    final code updated

--- routes/routes.js
+++ routes/routes.js
@@ -1,35 +1,93 @@
+// const path = require('path');
+// const fs = require('fs'); 
+
+// module.exports = (app) => {
+
+//     fs.readFile("db/db.json","utf8", (err, data) => {
+//         if (err) throw err;
+//         var notesData = JSON.parse(data);
+//     });
+
+//     app.get('/', (req,res) => {
+//         res.sendFile(path.join(__dirname, '../public/index.html'));
+//     });
+
+//     app.get('/notes', (req,res) => {
+//         res.sendFile(path.join(__dirname, '../public/notes.html'));
+//     })
+
+//     app.get("api/notes", (req,res) => {
+//         res.json(notesData);
+//     })
+
+//     app.get("/api/notes/:id", (req,res) => {
+//         res.json(notedata[req.params.id]);
+//     });
+
+//     app.post('api/notes', (req,res) => {
+//         let newNote = req.body
+//         notesData.push(newNote); 
+//         fs.writeFile("db/db.json", JSON.stringify(notes, '\t'), err => {
+//             if (err) throw err; 
+//             return true; 
+//         });
+//     });
+// }; 
+
+
+const fs = require('fs');
 const path = require('path');
-const fs = require('fs'); 
 
-module.exports = (app) => {
+module.exports = app => {
 
+    // Setup notes variable
     fs.readFile("db/db.json","utf8", (err, data) => {
+
         if (err) throw err;
-        var notesData = JSON.parse(data);
-    });
 
-    app.get('*', (req,res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
+        var notes = JSON.parse(data);
 
-    app.get('/notes', (req,res) => {
-        res.sendFile(path.join(__dirname, '../public/notes.html'));
-    })
-
-    app.get("api/notes", (req,res) => {
-        res.json(notesData);
-    })
-
-    app.get("/api/notes/:id", (req,res) => {
-        res.json(notedata[req.params.id]);
-    });
-
-    app.post('api/notes', (req,res) => {
-        let newNote = req.body
-        notesData.push(newNote); 
-        fs.writeFile("db/db.json", JSON.stringify(notes, '\t'), err => {
-            if (err) throw err; 
-            return true; 
+        // API ROUTES
+        // ========================================================
+    
+        // Setup the /api/notes get route
+        app.get("/api/notes", function(req, res) {
+            // Read the db.json file and return all saved notes as JSON.
+            res.json(notes);
         });
+
+        // Setup the /api/notes post route
+        app.post("/api/notes", function(req, res) {
+            // Receives a new note, adds it to db.json, then returns the new note
+            let newNote = req.body;
+            notes.push(newNote);
+            updateDb();
+            return console.log("Added new note: "+newNote.title);
+        });
+
+        // Retrieves a note with specific id
+        app.get("/api/notes/:id", function(req,res) {
+            // display json for the notes array indices of the provided id
+            res.json(notes[req.params.id]);
+        });
+
+        
+        app.get('/notes', function(req,res) {
+            res.sendFile(path.join(__dirname, "../public/notes.html"));
+        });
+        
+        app.get('*', function(req,res) {
+            res.sendFile(path.join(__dirname, "../public/index.html"));
+        });
+
+        //updates the json file whenever a note is added or deleted
+        function updateDb() {
+            fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
+                if (err) throw err;
+                return true;
+            });
+        }
+
     });
-}; 
+
+}
--- server.js
+++ server.js
@@ -1,34 +1,21 @@
-const fs = require ("fs");
+// Require Dependencies
+const express = require("express");
+const fs = require("fs");
+const path = require('path');
 
-// DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
-
-const express = require('express');
-
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
-
-// Tells node that we are creating an "express" server
+// Initialize express app
 const app = express();
-
-// Sets an initial port. We"ll use this later in our listener
 const PORT = process.env.PORT || 8000;
 
-// Sets up the Express app to handle data parsing
+// Setup data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname));
 
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-
-//require('./routes/apiRoutes')(app);
+//Require routes file
 require('./routes/routes')(app);
-//require('./routes/api/notes')(app);
 
-// LISTENER
-// The below code effectively "starts" our server
-
-app.listen(PORT, () => {
-  console.log(`App listening on PORT: ${PORT}`);
-});
+// Setup listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});  
